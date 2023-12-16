aa31108cd6b5b85b2a68a5d994669105d819210f
nadaism <putudesianada@gmail.com>
2023-12-16 23:12:50 +0700

    Code Keempat

--- Day 04/day04.py
+++ Day 04/day04.py
@@ -1,11 +1,8 @@
 """
-You are going to be given a word. 
-Your job is to return the middle character of the word. 
+You are going to be given a word. Your job is to return the middle character of the word. 
 If the word's length is odd, return the middle character. 
 If the word's length is even, return the middle 2 characters.
 """
-
-
 def get_middle(s):
     kalimat = len(s)
     return s[(kalimat - 1)//2 : (kalimat+2)//2]

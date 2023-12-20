d544ece05beb7f016f762dded16a1349195ece37
nadaism <putudesianada@gmail.com>
2023-12-20 19:46:37 +0700

    Code Kedelapan

---
+++ Day 08/day08.py
@@ -0,0 +1,12 @@
+def human_years_cat_years_dog_years(human_years):
+    if human_years == 1:
+        cat_years = 15
+        dog_years = 15
+    elif human_years == 2:
+        cat_years = 24
+        dog_years = 24
+    else:
+        cat_years = 24 + 4 * (human_years - 2)
+        dog_years = 24 + 5 * (human_years - 2)
+        
+    return [human_years,cat_years,dog_years]

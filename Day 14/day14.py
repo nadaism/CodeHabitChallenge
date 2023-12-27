cedca4385a8178c1bd208113d7d711b01fce8b06
nadaism <putudesianada@gmail.com>
2023-12-27 18:02:27 +0700

    Code Keempatbelas

--- Day 14/day14.py
+++ Day 14/day14.py
@@ -1,1 +1,14 @@
 
+"""
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+"""
+def odd_or_even(arr):
+    hasil = sum(arr)
+    if hasil %2 == 0:
+        return "even"
+    else:
+        return "odd"
